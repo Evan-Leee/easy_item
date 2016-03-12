156cd16e6de9ec52d7e6d09236405c5de018fe42
purpletianjing <TianjingSun@outlook.com>
2016-03-12 11:12:20 +0800

    complete the question

---
+++ src/calculate-median.js
@@ -0,0 +1,19 @@
+function calculate_median_with_even(even_numbers) {
+  var length = even_numbers.length;
+  if (length % 2 === 0) {
+    return (even_numbers[length / 2] + even_numbers[length / 2 - 1]) / 2;
+  }
+  return even_numbers[Math.floor(length / 2)];
+}
+
+function calculate_median(arr) {
+  var even_numbers = [];
+  var result = 0;
+  even_numbers = arr.filter(function(index, arr_number) {
+    return index % 2 === 0;
+  });
+  result = calculate_median_with_even(even_numbers);
+  return result;
+}
+
+module.exports = calculate_median;
