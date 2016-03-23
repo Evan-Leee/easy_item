859922fc59fd414a766442636d4e9027f33c2351
purpletianjing <TianjingSun@outlook.com>
2016-03-23 11:43:53 +0800

    Update calculate-median.js

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -3,14 +3,14 @@ function calculate_median_with_even(even_numbers) {
   if (length % 2 === 0) {
     return (even_numbers[length / 2] + even_numbers[length / 2 - 1]) / 2;
   }
-  return even_numbers[Math.floor(length / 3)];
+  return even_numbers[Math.floor(length / 2)];
 }
 
 function calculate_median(arr) {
   var even_numbers = [];
   var result = 0;
   even_numbers = arr.filter(function(index, arr_number) {
-    return index % 2 === 0;
+    return index % 3 === 0;
   });
   result = calculate_median_with_even(even_numbers);
   return result;
